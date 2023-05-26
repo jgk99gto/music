ca73db4b8e4688a6781b55be926403b3d2939798
jgk99gto <130435534+jgk99gto@users.noreply.github.com>
2023-05-26 23:15:55 +0300

    Update config.py

--- config.py
+++ config.py
@@ -13,15 +13,15 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/136378b420ddd7c45dea4.jpg")
-START_IMG = getenv("START_IMG", "https://telegra.ph/file/136378b420ddd7c45dea4.jpg")
+PING_IMG = getenv("PING_IMG", "https://graph.org/file/e4757dce330ce4887adad.jpg")
+START_IMG = getenv("START_IMG", "https://graph.org/file/e4757dce330ce4887adad.jpg")
 
 SESSION = getenv("SESSION", None)
 
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/ZZZ7iZ")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/aaaalqp")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ZZZ7iZ")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5656828413").split()))
 
 
-FAILED = "https://telegra.ph/file/136378b420ddd7c45dea4.jpg"
+FAILED = "https://graph.org/file/e4757dce330ce4887adad.jpg"
